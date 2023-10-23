6ef95b67298449ffaee3f479305765fce8c873f7
Stella Maia <stellamaia97@hotmail.com>
2023-10-23 12:50:32 -0300

    Exclusão de componente

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
   <v-app>
     <div>
       <CompanyRegister/>
-      <!-- <CompanyRegisterModal/> -->
     </div>
   </v-app>
 </template>
 
 <script>
 import CompanyRegister from './components/CompanyRegister.vue';
-// import CompanyRegisterModal from './components/CompanyRegisterModal.vue';
 export default {
   name: 'App',
   components: {
     CompanyRegister,
-    // // CompanyRegisterModal
   },
   data() {
   }
