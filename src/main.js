9b423d8131a8bb14122391d993b0b5974befd351
Stella Maia <stellamaia97@hotmail.com>
2023-10-23 12:47:41 -0300

    Implementação de nova biblioteca

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import VueMask from 'v-mask';
+Vue.use(VueMask); // Registre o VueMask globalmente
+
 Vue.config.productionTip = false
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
