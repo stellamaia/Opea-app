4378d5432914f1d5b607e48e3c8f25f59bde3b14
Stella Maia <stellamaia97@hotmail.com>
2023-10-23 01:39:51 -0300

    Implementações finais

--- src/components/CompanyRegister.vue
+++ src/components/CompanyRegister.vue
@@ -5,13 +5,13 @@
         <div class="container-input">
             <div class="input-content">
 
-                <form @submit.prevent="searchCompanies">
+                <v-form ref="form" @submit.prevent="searchCompanies">
                     <input v-model="searchText" class="form-control" type="search" placeholder="Buscar empresa..."
                         aria-label="Search">
                     <button class="btn-search" type="submit">
                         <img class="img-search" src="@/assets/search.png" alt="Search Icon">
                     </button>
-                </form>
+                </v-form>
 
             </div>
 
@@ -20,38 +20,46 @@
                 <div class="rounded-icon-btn">
                     <img class="list-icon" src="@/assets/list.png" alt="Add Icon">
                 </div>
-                <v-btn class="add-btn" @click="dialog = true" id="meuBotao">Adicionar Empresa</v-btn>
+                <v-btn class="add-btn " @click="dialog = true" id="meuBotao">Adicionar Empresa</v-btn>
+            </div>
+            <div v-if="companies.length > 0">
+
+                <div v-for="company in companies" :key="company.id" class="add-company-input">
+
+                    <div @click="dialog = true"  class="add-btn-input" id="meuBotao">
+                        <div class="rounded-icon-input">
+                            <img class="list-icon-input" src="@/assets/list.png" alt="Add Icon">
+                        </div>
+                        <div class="content-items">
+                            <p class="item company">Nome: {{ company.name }}</p>
+                            <div class="item-flex">
+                                <p class="item">CNPJ: {{ company.cnpj }}</p>
+                                <p class="item">Email: {{ company.email }}</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-else class="search-company">
+
+                <p>Sem empresa para mostrar</p>
+                <span class="material-symbols-outlined">
+                    block
+                </span>
             </div>
 
-
-            <div v-for="company in companies" :key="company.id" class="add-company-input">
-                <div class="rounded-icon-input">
-                    <img class="list-icon-input" src="@/assets/list.png" alt="Add Icon">
-                </div>
-                <div class="add-btn-input" id="meuBotao">
-                    <p class="item">Nome: {{ company.name }}</p>
-                 <div class="item-flex">
-                    <p class="item">CNPJ: {{ company.cnpj }}</p>
-                    <p class="item">Email: {{ company.email }}</p>
-
-                 </div>
-                </div>
-
-            </div>
-
-            <!-- <div v-else>
-                <p>Não há empresas para exibir</p>
-            </div> -->
         </div>
+
 
         <v-dialog v-model="dialog" persistent max-width="390">
             <v-card>
-                <v-card-title class="content-card">
+                <div class="content-card">
                     <v-card-title class="title-modal">Cadastrar Empresa</v-card-title>
                     <v-btn class="close-btn">
                         <v-img class="close-modal-register" @click="closeDialog" src="../assets/close.png"></v-img>
                     </v-btn>
-                </v-card-title>
+                </div>
 
                 <v-divider class="divider"></v-divider>
                 <v-card-text>
@@ -69,12 +77,15 @@
                                 <label class="label">E-mail</label>
                                 <input class="input" type="text" id="email" v-model="email" />
                             </v-col>
+                            <v-alert v-if="showAlert" border="left" color="red" type="error">
+                                Preenchaos os campos
+                            </v-alert>
                         </v-row>
                     </v-form>
                 </v-card-text>
                 <v-card-actions class="content-btn">
 
-                    <v-btn color="delete-btn " @click="dialog = false">
+                    <v-btn color="delete-btn " @click="excluirEmpresa(company.id)">
                         <img class="delete-btn-img" src="../assets/delete.png" />
                     </v-btn>
 
@@ -105,6 +116,9 @@ export default {
             nome: '',
             email: '',
             cnpj: '',
+            showAlert: false, // Initially, the alert is hidden
+            preventRegistration: false, // Initially, registration is not prevented
+            // ... other data properties ...
             companies: [],
             filteredCompanies: [],
             headers: [
@@ -116,38 +130,75 @@ export default {
     },
 
     methods: {
-        // openDialog() {
-        //     this.dialog = true;
-        // },
+     openDialog() {
+         this.dialog = true;
+     },
         closeDialog() {
             this.dialog = false;
             this.resetForm();
+            this.resetAlert();
+
         },
-        // resetForm() {
-        //     this.nome = "";
-        //     this.cnpj = "";
-        //     this.email = "";
-        //     this.$refs.form.resetValidation();
-        // },
+
+        resetAlert() {
+            this.showAlert = false; // Hide the alert
+        },
+        resetForm() {
+            this.nome = "";
+            this.cnpj = "";
+            this.email = "";
+            this.$refs.form.resetValidation();
+        },
         searchCompanies() {
             axios.get(`https://outros.opea-uat.solutions/prova/front/api/clients?text=${this.searchText}`).then((response) => {
                 this.companies = response.data;
                 // console.log(response.data);
             });
         },
-        // createCompany() {
-        //     this.$refs.form.validate();
-        //     if (this.valid) {
-        //         axios.post(`https://outros.opea-uat.solutions/prova/front/api/clients`, {
-        //             name: this.nome,
-        //             cnpj: this.cnpj,
-        //             email: this.email,
-        //         }).then((response) => {
-        //             this.companies.push(response.data);
-        //             this.closeDialog();
-        //         });
-        //     }
-        // },
+        createCompany() {
+            if (this.nome.trim() === '' || this.cnpj.trim() === '' || this.email.trim() === '') {
+                this.showAlert = true; // Show the alert
+                this.preventRegistration = true; // Prevent registration
+            } else {
+                this.showAlert = false; // Hide the alert
+                this.preventRegistration = false; // Allow registration
+                this.$refs.form.validate();
+                if (this.valid) {
+                    axios.post(`https://outros.opea-uat.solutions/prova/front/api/clients`, {
+                        name: this.nome,
+                        cnpj: this.cnpj,
+                        email: this.email,
+                    }).then((response) => {
+                        this.companies.push(response.data);
+                        this.closeDialog();
+                    });
+                }
+            }
+        },
+
+
+        editarEmpresa(empresa) {
+        this.id = empresa.id;
+        this.nome = empresa.name;
+        this.cnpj = empresa.cnpj;
+        this.email = empresa.email;
+        this.dialog = true; // Abra o diálogo para edição
+    },
+        excluirEmpresa(id) {
+  const confirmarExclusao = confirm('Tem certeza de que deseja excluir esta empresa?');
+  if (confirmarExclusao) {
+    axios.delete(`https://outros.opea-uat.solutions/prova/front/api/clients/${id}`)
+      .then(() => {
+        // Remova a empresa excluída da lista
+        this.companies = this.companies.filter(company => company.id !== id);
+      })
+      .catch(error => {
+        console.error('Erro ao excluir a empresa:', error);
+      });
+  }
+}
+
+
     }
 
 }
@@ -155,13 +206,25 @@ export default {
 
 
 <style scoped >
+* {
+    outline: none;
+}
+
+input:focus {
+    outline: none;
+}
+
 .company-register {
     background-color: #F5F5F5;
     height: 100vh;
 }
 
 .container-input {
-    padding: 50px 30px 0 30px;
+    padding: 50px 30px 50px 30px;
+    background-color: #F5F5F5;
+
+
+
 }
 
 .v-card.v-sheet.theme--light {
@@ -182,7 +245,7 @@ export default {
     top: 180px;
     height: 20px;
     right: 40px;
-    position: fixed;
+    position: absolute;
 }
 
 .add-company-btn {
@@ -193,6 +256,7 @@ export default {
 .add-company-input {
     padding-top: 10px;
     background-color: #F5F5F5 !important;
+
 }
 
 .add-btn {
@@ -210,26 +274,41 @@ export default {
 .add-btn-input {
     height: 50px !important;
     width: 100%;
+    display: flex;
     box-shadow: none;
     border: 1.3px solid rgb(217, 217, 217);
     background-color: #ffffff !important;
     border-radius: 30px 6px 6px 30px;
     font-size: 14.5px !important;
     font-weight: 500 !important;
-    color: #969696 !important;
-    padding-left: 70px;
-    padding-top: 7px;
+    color: #616161 !important;
+    padding-left: 8px;
+    padding-top: 2px;
 
 }
-.item{
-font-size: 12px;
-margin: 0;
-padding-right: 10px;
+
+.content-items {
+    padding-left: 15px;
+
+    padding-top: 6px;
 }
 
-.item-flex{
+.item {
+    font-size: 10px;
+    margin: 0;
+    padding-right: 10px;
+}
+
+.company {
+    font-size: 12.5px !important;
+    font-weight: 500 !important;
+}
+
+.item-flex {
     display: flex;
+    padding-top: 1px;
 }
+
 .v-btn {
     text-transform: none;
 }
@@ -250,10 +329,10 @@ padding-right: 10px;
 }
 
 .rounded-icon-input {
-    position: absolute;
+    /* position: absolute;
     top: 338px;
     left: 39px;
-    z-index: 1;
+    z-index: 1; */
     height: 40px;
     width: 40px;
     border-radius: 50%;
@@ -262,6 +341,8 @@ padding-right: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-top: 2px;
+
 }
 
 .list-icon {
@@ -276,16 +357,15 @@ padding-right: 10px;
 
 .title-modal {
     color: #969696;
-    font-size: 13px !important;
+    font-size: 15px !important;
+    font-weight: 400;
 }
 
-.content-card {
-    display: flex;
-}
 
 .close-modal-register {
     height: 50px;
     width: 50px;
+
 }
 
 .content-card {
@@ -341,6 +421,7 @@ padding-right: 10px;
 .btn-register {
     background-color: #630A37 !important;
     color: white;
+
 }
 
 .divider {
@@ -350,12 +431,24 @@ padding-right: 10px;
 }
 
 .close-btn {
-    background-color: white !important;
+    background-color: rgba(255, 255, 255, 0) !important;
     box-shadow: none;
     width: 0px;
+    padding-top: 30px !important;
+    margin-right: 10px !important;
 }
 
-::v-deep .close-btn:hover {
-    background-color: rgba(201, 159, 159, 0) !important;
+.close-btn::before {
+    background-color: transparent;
+    height: 20px;
+    margin-top: 20px;
+}
+
+.search-company {
+    text-align: center;
+    font-size: 13px !important;
+    font-weight: bold !important;
+    color: #969696 !important;
+    padding-top: 20px;
 }
 </style>
--- src/components/CompanyRegisterModal.vue
+++ src/components/CompanyRegisterModal.vue
@@ -1,98 +1,120 @@
 <template>
-    <v-row justify="center">
-        <v-dialog v-model="dialog" persistent max-width="600px">
+    <div>
+        <NavBar />
 
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                    Open Dialog
-                </v-btn>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <v-text-field
+                        v-model="searchText"
+                        label="Pesquisar Empresas"
+                        append-icon="mdi-magnify"
+                        @click:append="searchCompanies"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
 
-            </template>
+            <v-row>
+                <v-col cols="12">
+                    <v-data-table :headers="headers" :items="companies" :search="searchText"></v-data-table>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12">
+                    <v-btn color="btn-register" @click="openDialog">Cadastrar Empresa</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <v-dialog v-model="dialog" max-width="600px">
             <v-card>
                 <v-card-title>
-                    <span class="text-h5">Cadastrar empresa</span>
-                    <v-spacer></v-spacer>
-                    <v-btn text @click="dialog = false">
-                        <v-img src="../assets/close.png"></v-img>
-                    </v-btn>
-
+                    <span class="text-h5">Cadastrar Empresa</span>
                 </v-card-title>
-                <v-divider></v-divider>
                 <v-card-text>
-                    <v-container>
-
+                    <v-form ref="form" v-model="valid" lazy-validation>
                         <v-row>
                             <v-col cols="12">
-                                <v-text-field label="Nome" required></v-text-field>
+                                <label class="label">Nome</label>
+                                <input class="input" type="text" id="name" v-model="nome" />
                             </v-col>
                             <v-col cols="12">
-                                <v-text-field label="CNPJ" type="password" required></v-text-field>
+                                <label class="label label-cnpj">CNPJ</label>
+                                <input class="input input-cnpj" type="text" id="cnpj" v-model="cnpj" />
                             </v-col>
                             <v-col cols="12">
-                                <v-text-field label="E-mail" required></v-text-field>
+                                <label class="label">Email</label>
+                                <input class="input" type="text" id="email" v-model="email" />
                             </v-col>
-
                         </v-row>
-                    </v-container>
+                    </v-form>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="dialog = false">
-                        <v-img class="icon-delete" src="../assets/delete.png"></v-img>
-                    </v-btn>
-
-                    <v-btn color="blue darken-1" text @click="dialog = false">
-                        Cancelar </v-btn>
-                    <v-btn color="blue darken-1" text @click="dialog = false">
-                        Cadastrar
-                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="closeDialog">Cancelar</v-btn>
+                    <v-btn color="blue darken-1" text @click="createCompany">Cadastrar</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
-    </v-row>
+    </div>
 </template>
 
 <script>
+import NavBar from "./NavBar";
+import axios from "axios";
 export default {
-    name: 'App',
+    components: {
+        NavBar,
+    },
     data() {
         return {
-            dialog: false
-        }
-    }
+            searchText: "",
+            dialog: false,
+            valid: true,
+            nome: "",
+            cnpj: "",
+            email: "",
+            companies: [],
+            headers: [
+                { text: "Nome", value: "name" },
+                { text: "CNPJ", value: "cnpj" },
+                { text: "Email", value: "email" },
+            ],
+        };
+    },
+    methods: {
+        openDialog() {
+            this.dialog = true;
+        },
+        closeDialog() {
+            this.dialog = false;
+            this.resetForm();
+        },
+        resetForm() {
+            this.nome = "";
+            this.cnpj = "";
+            this.email = "";
+            this.$refs.form.resetValidation();
+        },
+        searchCompanies() {
+            axios.get(`https://outros.opea-uat.solutions/prova/front/api/clients?text=${this.searchText}`).then((response) => {
+                this.companies = response.data;
+            });
+        },
+        createCompany() {
+            this.$refs.form.validate();
+            if (this.valid) {
+                axios.post(`https://outros.opea-uat.solutions/prova/front/api/clients`, {
+                    name: this.nome,
+                    cnpj: this.cnpj,
+                    email: this.email,
+                }).then((response) => {
+                    this.companies.push(response.data);
+                    this.closeDialog();
+                });
+            }
+        },
+    },
 };
-</script>
-<style>
-.icon-delete {
-    width: 1px;
-}
-
-.v-dialog {
-    overflow: hidden;
-
-}
-
-.v-card.v-sheet.theme--light {
-    padding: 20px;
-}
-
-.v-btn:before {
-    background-color: rgba(255, 255, 255, 0);
-}
-
-.v-btn:hover {
-    background-color: rgba(255, 255, 255, 0);
-}
-
-.v-input__slot {
-    border: 1px solid rgb(137, 137, 137);
-    border-radius: 4px !important;
-    height: 50px;
-}
-
-.v-text-field input {
-    padding: 8px;
-}
-
-
-</style>
+</script> 
